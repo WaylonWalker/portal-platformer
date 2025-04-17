99b7c328a70aad22e360110e8351c9e4257fb252
Waylon S. Walker <waylon@waylonwalker>
2025-04-16 19:31:06 -0500

    upgrade cli to typer

--- portal_platformer/cli.py
+++ portal_platformer/cli.py
@@ -1,23 +1,38 @@
+import typer
+
 from portal_platformer.game import Game
+from portal_platformer.server import Server
+
+app = typer.Typer()
 
 
-def main():
-    # use argparse instead
-    # args = {}
-    # if "--debug" in sys.argv:
-    #     args["debug"] = True
-    # if "--fullscreen" in sys.argv:
-    #     args["fullscreen"] = True
-
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--debug", action="store_true")
-    parser.add_argument("--fullscreen", action="store_true")
-    parser.add_argument("--width", type=int, default=1920)
-    parser.add_argument("--height", type=int, default=1080)
-    parser.add_argument("--map", type=str, default="test")
-    args = vars(parser.parse_args())
+@app.command()
+def run(
+    debug: bool = False,
+    fullscreen: bool = False,
+    width: int = 1920,
+    height: int = 1080,
+    map: str = "test",
+    save_file: str = "save_state",
+):
+    args = {
+        "debug": debug,
+        "fullscreen": fullscreen,
+        "width": width,
+        "height": height,
+        "map": map,
+        "save_file": save_file,
+    }
 
     game = Game(**args)
     game.run()
+
+
+@app.command()
+def server():
+    server = Server()
+    server.run()
+
+
+if __name__ == "__main__":
+    app()
